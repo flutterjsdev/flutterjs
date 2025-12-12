ee07832e04d5ae9e5b2165f96b29b749b4dccf37
Jayprakash Pal <jaypal1046@gmail.com>
2025-12-12 22:19:40 +0530

    -- improving folder structure

---
+++ packages/flutterjs_engine/package/material/src/utils/utils.js
@@ -0,0 +1,75 @@
+// index.js (or index.ts) - Barrel file for all design system utilities
+
+// ===================================================================
+// Utils (added 2 days ago)
+// ===================================================================
+export * from './property/alignment.js';
+export * from './property/axis.js';
+export * from './border.js';
+export * from './border_radius.js';
+export * from './property/border_style.js';
+export * from './property/box_fit.js';
+export * from './box_shadow.js';
+export * from './property/clip.js';
+export * from './color.js';
+export * from './property/cross_axis_alignment.js';
+export * from './duration.js';
+export * from './edge_insets.js';
+export * from './property/font_weight.js';
+export * from './input_decoration.js';
+export * from './linear_gradient.js';
+export * from './property/main_axis_alignment.js';
+export * from './offset.js';
+export * from './radius.js';
+export * from './rect.js';
+export * from './size.js';
+export * from './property/text_align.js';
+export * from './property/text_overflow.js';
+export * from './text_style.js';
+export * from './property/text_direction.js';
+
+
+// ===================================================================
+// Themes (updated with new properties)
+// ===================================================================
+export * from './badge_theme.js';
+export * from './checkbox_theme.js';
+export * from './colors_theme.js';
+export * from './divider_theme.js';
+export * from './motion_theme.js';
+export * from './scrollbar_theme.js';
+export * from './shadows_theme.js';
+export * from './switch_theme.js';
+export * from './typography_theme.js';
+
+
+// ===================================================================
+// New Properties Added (yesterday)
+// ===================================================================
+export * from './property/axis_direction.js';
+export * from './property/blend_mode.js';
+export * from './property/box_shape.js';
+export * from './property/decoration_position.js';
+export * from './property/filter_quality.js';
+export * from './property/flex_fit.js';
+export * from './property/font_style.js';
+export * from './property/horizontal_direction.js';
+export * from './property/keyboard_type.js';
+export * from './property/main_axis_size.js';
+export * from './property/shape_border.js';
+export * from './property/stack_fit.js';
+export * from './property/text_baseline.js';
+export * from './property/text_decoration.js';
+export * from './property/text_direction.js';
+export * from './property/text_input_action.js';
+export * from './property/text_input_type.js';
+export * from './property/vertical_direction.js';
+export * from './property/wrap_alignment.js';
+export * from './property/wrap_cross_alignment.js';
+
+
+// ===================================================================
+// Other / Misc
+// ===================================================================
+export * from './property/other.js';
+export * from './geometry.js';
