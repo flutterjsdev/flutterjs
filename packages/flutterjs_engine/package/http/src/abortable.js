0542d490ce9b0fac2a99ecf53243923a2ca4415f
Jayprakash Pal <jaypal1046@gmail.com>
2026-01-10 22:41:48 +0530

    chore: Finalize http package implementation and tests

--- packages/flutterjs_engine/package/http/src/abortable.js
+++ packages/flutterjs_engine/package/http/src/abortable.js
@@ -1,5 +1,26 @@
 
-// Shim for src/abortable.dart
+/**
+ * A mixin that can be used to abort an operation.
+ * 
+ * In the JavaScript implementation, this wraps AbortController.
+ */
 export class Abortable {
-    // Basic shim
+    constructor() {
+        this._controller = new AbortController();
+    }
+
+    /**
+     * Aborts the operation.
+     */
+    abort() {
+        this._controller.abort();
+    }
+
+    /**
+     * Returns the AbortSignal associated with this abortable.
+     * Internal use for passing to fetch/axios.
+     */
+    get signal() {
+        return this._controller.signal;
+    }
 }
